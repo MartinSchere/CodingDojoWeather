4a3a3aa55d511ae475aa945777f22063b4ca6041
abecerraguz <alejandro.becerra@publimetro.cl>
2021-06-30 20:12:06 -0400

    Completado

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-const API_KEY = "65cf18766bf746e38ca224944213006";
+const API_KEY = "65cf18766bf746e38ca224944213006F";
 
 // DOCS: https://www.weatherapi.com/docs/
-$.ajax(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London&aqi=no`).done((data) =>
-  console.log(data)
-);
+
+
+
+$(document).ready(function(){
+
+  $('#find').click(function(){
+    console.log('click');
+
+    $.ajax(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Santiago&aqi=no`).done((data) =>{
+
+      console.log(data)
+      let {cloud,feelslike_c,humidity,temp_c,last_updated,condition} = data.current
+
+      let card = $('#cardContent').find('div');
+
+      card.find('.card-title').text(`${temp_c} Grados`);
+      card.find('.card-title').append(`<img src="${condition.icon}"/>`);
+      card.find('.card-subtitle').text(`${humidity}% de Humedad`);
+      card.find('.card-text').text(`Actualizado el ${last_updated}`);
+      console.log(card);
+
+      $('#cardContent').removeClass('d-none')
+
+    }).fail(()=>{
+      $('#error').append(`<div class="alert alert-danger" role="alert">
+          Error!
+       </div>`)
+    });
+
+  })
+
+})
